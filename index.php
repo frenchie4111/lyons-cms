f4ae044e2a11ea6f05bd36da0dc67e117fd7cb46
Mike Lyons <mlyons@apple.com>
2013-06-23 10:33:02 -0700

    Removed direct links to stylesheets from index and portal

--- index.php
+++ index.php
@@ -16,8 +16,7 @@ $results = mysqli_query( $con, "SELECT * FROM pictures ORDER BY position ASC");
 	<link href='http://mikelyons.org/reset.css' rel='stylesheet' type='text/css'>
 	<link href='http://fonts.googleapis.com/css?family=Shanti' rel='stylesheet' type='text/css'>
 
-	<link href='http://hodenfield.mikelyons.org/admin/main.css' rel='stylesheet' type='text/css'>
-	<link href='http://hodenfield.mikelyons.org/admin/pictures/pictures.css' rel='stylesheet' type='text/css'>
+	<link href='main.css' rel='stylesheet' type='text/css'>
 
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js"></script>
